dce7c8c578d520985c4549625d5c48d64e5271bb
Enzo Couto <enzo@gec.inatel.br>
2025-05-07 13:21:27 -0300

    Corrigindo interface

--- src/interface/main.py
+++ src/interface/main.py
@@ -22,7 +22,6 @@ def main():
     # Centralized title
     st.title("Sistema de Controle distribuído para Branqueamento Térmico da Glicerina com controlador PID")
 
-    # Default values for the right column graph
     initial_k, initial_theta, initial_tau = 0.09, 5.17, 47.25
     arquivo_mat = 'Dataset/Dataset_Grupo6.mat'
     try:
@@ -34,24 +33,23 @@ def main():
 
         amplitude_degrau = degrau[-1]  # Step amplitude
 
-        # *** Seção da antiga coluna esquerda ***
         st.subheader("Dados do circuito")
 
-        with st.container():  # Removi height=350
-            plot_title_graph_left = st.empty()  # Placeholder for the graph title
-            plot_left_placeholder = st.empty()  # Placeholder for the graph
+        with st.container():  
+            plot_title_graph_first = st.empty()  # Placeholder for the graph title
+            plot_first_placeholder = st.empty()  # Placeholder for the graph
 
         # Initialize the graph with the default selectbox option
         graph_options = ("Dataset", "Malha aberta", "Malha fechada")
         option = st.selectbox("Escolha o gráfico:", graph_options, index=0)
 
-        plot_title_graph_left.subheader(f"{option}")
+        plot_title_graph_first.subheader(f"{option}")
 
         # Calculate and plot the open loop
         K, tau_estimado, theta_estimado = estimate_pid_values(tempo, degrau, potencia_motor)
 
         if option == "Dataset":
-            fig_left = plot_dataset_graph_plotly(tempo, degrau, potencia_motor, "Dataset")
+            fig_first = plot_dataset_graph_plotly(tempo, degrau, potencia_motor, "Dataset")
 
         elif option == "Malha aberta":
             # Create the transfer function for the open loop
@@ -65,7 +63,7 @@ def main():
             tempo_open, saida_open = ctl.step_response(open_loop_with_theta, tempo)
 
             # Create plot for the open loop
-            fig_left = plot_graph_open_loop_plotly(
+            fig_first = plot_graph_open_loop_plotly(
                 tempo_open, saida_open, tempo, degrau, "Resposta em Malha Aberta"
             )
 
@@ -84,25 +82,24 @@ def main():
             tempo_closed, saida_closed = ctl.step_response(closed_loop, tempo)
 
             # Create plot for the closed loop
-            fig_left = plot_graph_closed_loop_plotly(
+            fig_first = plot_graph_closed_loop_plotly(
                 tempo_closed, saida_closed, tempo, degrau, "Resposta em Malha Fechada"
             )
 
         # Display the graph in the placeholder
-        plot_left_placeholder.plotly_chart(fig_left, use_container_width=True)
+        plot_first_placeholder.plotly_chart(fig_first, use_container_width=True)
 
-        # *** Seção da antiga coluna direita ***
         st.subheader("Gráfico do controlador PID")
 
-        # Container for the graph with a fixed height
-        with st.container():  # Removi height=350
-            plot_title_graph_right = st.empty()  # Placeholder for the graph title
-            plot_right_placeholder = st.empty()  # Placeholder for the graph
+       
+        with st.container(): 
+            plot_title_graph_second = st.empty()  # Placeholder for the graph title
+            plot_second_placeholder = st.empty()  # Placeholder for the graph
 
         # Selectbox to choose the type of PID
         pid_options = ("CHR", "ITAE")
         option2 = st.selectbox("Especifique o Método de Sintonia:", pid_options, index=0)
-        plot_title_graph_right.subheader(f"{option2}")
+        plot_title_graph_second.subheader(f"{option2}")
 
     
         st.write("Parâmetros do PID")
@@ -128,13 +125,13 @@ def main():
         # Update the graph based on the selected option
         if option2 == "CHR":
             tempo_chr, saida_chr = CHR(k, tau_i, theta_d, amplitude_degrau, tempo)
-            fig_right = plot_graph_pid_plotly(tempo_chr, saida_chr, tempo, option2, degrau)
+            fig_second = plot_graph_pid_plotly(tempo_chr, saida_chr, tempo, option2, degrau)
         elif option2 == "ITAE":
             tempo_itae, saida_itae = ITAE(k, tau_i, theta_d, amplitude_degrau, tempo)
-            fig_right = plot_graph_pid_plotly(tempo_itae, saida_itae, tempo, option2, degrau)
+            fig_second = plot_graph_pid_plotly(tempo_itae, saida_itae, tempo, option2, degrau)
 
         # Update the graph in the placeholder
-        plot_right_placeholder.plotly_chart(fig_right, use_container_width=True)
+        plot_second_placeholder.plotly_chart(fig_second, use_container_width=True)
 
     except FileNotFoundError:
         st.error(f"Erro: Arquivo '{arquivo_mat}' não encontrado.")
